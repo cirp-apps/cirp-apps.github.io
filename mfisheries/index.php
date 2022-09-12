96cfc5c2c46bf89ba471887bc624c2d568996c4f
cirp-apps <105650980+cirp-apps@users.noreply.github.com>
2022-09-12 09:06:40 -0400

    Updated mfisheries links

--- mfisheries/index.php
+++
@@ -1,4 +0,0 @@
-Database connection error (1): The MySQL adapter 'mysql' is not available.<br>
-<b>Warning</b>:  session_write_close(): Cannot call session save handler in a recursive manner in <b>C:\Development\xampp_php7\htdocs\cirpwp2\mfisheries\libraries\joomla\session\session.php</b> on line <b>676</b><br>
-<br>
-<b>Warning</b>:  session_write_close(): Failed to write session data using user defined save handler. (session.save_path: C:\Development\xampp_php7\tmp) in <b>C:\Development\xampp_php7\htdocs\cirpwp2\mfisheries\libraries\joomla\session\session.php</b> on line <b>676</b><br>
